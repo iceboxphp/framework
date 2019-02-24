839be40f60144a38f9c30ef9da2f8a6d6cb18075
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-02-24 15:52:25 +0600

    wip: crud generator, view/edit

--- src/generator/view/_form.html.php
+++ src/generator/view/_form.html.php
@@ -12,10 +12,10 @@ use Icebox\App;
     <input type="hidden" name="_method" value="<?php php_start_tag() ?> echo $method; <?php php_end_tag() ?>">
   <?php php_start_tag(); ?> } <?php php_end_tag(); ?>
 
-  <?php php_start_tag(); ?> if($post->errors != null) { <?php php_end_tag(); ?>
+  <?php php_start_tag(); ?> if($<?php echo $singular ?>->errors != null) { <?php php_end_tag(); ?>
 
     <div class="error-messages">
-      <?php php_start_tag(); ?> foreach($post->errors->full_messages() as $message) { <?php php_end_tag(); ?>
+      <?php php_start_tag(); ?> foreach($<?php echo $singular ?>->errors->full_messages() as $message) { <?php php_end_tag(); ?>
 
         <div class="text-danger"><?php php_start_tag(); ?> echo h($message); <?php php_end_tag(); ?></div>
       <?php php_start_tag(); ?> } <?php php_end_tag(); ?>
@@ -24,15 +24,15 @@ use Icebox\App;
   <?php php_start_tag(); ?> } <?php php_end_tag(); ?>
 
   <div class="form-group">
-      <label for="post_title_input">Title</label>
-      <input type="text" class="form-control" id="post_title_input" name="title"
-          aria-describedby="title_help" placeholder="Title" value="<?php php_start_tag(); ?> echo h($post->title); <?php php_end_tag(); ?>">
+      <label for="<?php echo $singular ?>_title_input">Title</label>
+      <input type="text" class="form-control" id="<?php echo $singular ?>_title_input" name="title"
+          aria-describedby="title_help" placeholder="Title" value="<?php php_start_tag(); ?> echo h($<?php echo $singular ?>->title); <?php php_end_tag(); ?>">
       <!-- <small id="title_help" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
     </div>
     <div class="form-group">
-      <label for="post_content_input">Content</label>
-      <textarea class="form-control" id="post_content_input" name="content"
-          placeholder="Content"><?php php_start_tag(); ?> echo h($post->content); <?php php_end_tag(); ?></textarea>
+      <label for="<?php echo $singular ?>_content_input">Content</label>
+      <textarea class="form-control" id="<?php echo $singular ?>_content_input" name="content"
+          placeholder="Content"><?php php_start_tag(); ?> echo h($<?php echo $singular ?>->content); <?php php_end_tag(); ?></textarea>
     </div>
 
     <button type="submit" class="btn btn-primary"><?php php_start_tag(); ?> echo $button_text; <?php php_end_tag(); ?></button>
---
+++ src/generator/view/edit.html.php
@@ -0,0 +1,23 @@
+<?php php_start_tag(); ?>
+
+use Icebox\App;
+<?php php_end_tag(); ?>
+
+
+This is <?php echo $plural ?>#edit page <br><br>
+
+File location: <?php echo __FILE__ ?>
+
+
+<?php php_start_tag(); ?>
+
+$this->render('_form', array(
+  '<?php echo $singular ?>' => $<?php echo $singular ?>,
+  'action' => App::url('/<?php echo $plural ?>/:id', array(':id' => $<?php echo $singular ?>->id)),
+  'method' => 'put',
+  'button_text' => 'Update'
+));
+<?php php_end_tag(); ?>
+
+
+<p><a href="<?php php_start_tag(); ?> echo App::url('/<?php echo $plural ?>'); <?php php_end_tag(); ?>">Back</a></p>
